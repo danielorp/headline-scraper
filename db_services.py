1e2308399139202292de88476d6fdba6ca886299
Daniel Orpinelli <danielorp@hotmail.com>
2019-07-26 01:35:22 +0000

    extrair hora de publicação e modificação de cada matéria

--- db_services.py
+++ db_services.py
@@ -1,14 +1,26 @@
 import sqlite3
 import peewee
-from models import Scraping
 from difflib import SequenceMatcher
+
+
+db = peewee.SqliteDatabase('scraping.db')
+
+
+class Materia(peewee.Model):
+    manchete = peewee.CharField()
+    url = peewee.CharField()
+    datahora = peewee.DateTimeField()
+
+    class Meta:
+        database = db
+
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 
 def compara_manchetes(manchete):
-        query_all = Scraping.select()
+        query_all = Materia.select()
         for row in query_all:
                 rate = similar(row.manchete, manchete)
                 if rate > 0.90:
--- scraping.py
+++ scraping.py
@@ -1,34 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
 import sqlite3
-from datetime import datetime
+from datetime import datetime, timedelta
 from pytz import timezone
 import peewee
-from models import Scraping
-from db_services import compara_manchetes
+from db_services import compara_manchetes, Materia
+
+
+def insereMateriaCompleta(manchete, url, data):
+        insert = Materia.create(manchete=manchete, url=url, datahora=data)
+        return
+
+
+def formatData(data):
+        year = int(data[:4])
+        month = int(data[5:7])
+        day = int(data[8:10])
+        hour = int(data[11:13])
+        minute = int(data[14:16])
+        dt = datetime(year, month, day, hour, minute) - timedelta(hours=3)
+        return dt
+
+
+def extraiData(url):
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        materiacompl = soup.find_all('time')
+        for mt in materiacompl:
+                if mt['itemprop'] == 'datePublished':
+                        dataPublic = formatData(mt['datetime'])
+                        return dataPublic
+                #if mt['itemprop'] == 'dateModified':
+                #        dataModified = formatData(mt['datetime'])
+                #        dataModified
+
+######################################################################################################
 
 # Certifica-se da existência da tabela.
-Scraping.create_table()
+Materia.create_table()
 
-page = requests.get('https://g1.globo.com')
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
+search = requests.get('https://g1.globo.com')
+search = BeautifulSoup(search.content, 'html.parser')
 # Permite busca com múltiplas classes na tag
 # tag = soup.find_all('a', {'class': 'feed-post-link', 'class': 'gui-color-primary', 'class': 'gui-color-hover'})
+search = search.find_all('a', class_="feed-post-link")
 
-search = soup.find_all('a', class_="feed-post-link")
-
-data_e_hora_atuais = datetime.now()
-fuso_horario = timezone('America/Sao_Paulo')
-data_e_hora_sao_paulo = data_e_hora_atuais.astimezone(fuso_horario)
-dt_hr_sp_texto = data_e_hora_sao_paulo.strftime('%d/%m/%Y %H:%M')
 
 for s in search:
-    manchete = s.contents[0]
-    url = s['href']
-    data = dt_hr_sp_texto
-    localizado = compara_manchetes(manchete)
-    if not localizado:
-        insert = Scraping.create(manchete=manchete, url=url, datahora=data)
-        print('Nova adição!')
+        manchete = s.getText()
+        url = s['href']
+        data = extraiData(url)
+        localizado = compara_manchetes(manchete)
+        if not localizado:
+                insereMateriaCompleta(manchete, url, data)
+                print('Nova adição!')
+
